96725215ce38022de0b3cf5093deaf8bc83b94b0
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-11-12 08:50:36 +0100

    ( ⇆ ) Moved components logic into a hook

--- components/Tracker/UserLocationTracker.tsx
+++ components/Tracker/UserLocationTracker.tsx
@@ -1,44 +1,22 @@
-import { useEffect, useState } from "react";
 import UserMarker from "../Markers/UserMarker";
-import { watchGeoLocation } from "@/lib/utils";
 
-interface LocationCoords {
-  long: number | null;
-  lat: number | null;
+interface UserLocationTrackerProps {
+  coordinates: Coordinates;
+  isTracking: boolean;
 }
 
-const UserLocationTracker: React.FC = () => {
-  // callback needed for watchLocation
-  const [coordinates, setCoordinates] = useState<LocationCoords>({
-    long: null,
-    lat: null,
-  });
-
-  const handleLocationUpdate = ({
-    long,
-    lat,
-  }: {
-    long: number;
-    lat: number;
-  }) => {
-    setCoordinates({ long, lat });
-  };
-
-  useEffect(() => {
-    const stopTracking = watchGeoLocation(handleLocationUpdate);
-
-    return () => {
-      stopTracking();
-      console.log("Unmount : tracking stopped");
-    };
-  }, []);
-
+const UserLocationTracker: React.FC<UserLocationTrackerProps> = ({
+  coordinates,
+  isTracking,
+}) => {
   const isLocationReady = coordinates.lat !== null && coordinates.long !== null;
 
   // conversion to Marker "position" data type
   const markerPosition: [number, number] | null = isLocationReady
     ? [coordinates.lat!, coordinates.long!]
     : null;
+
+  if (!isTracking) return null;
 
   return (
     <>
