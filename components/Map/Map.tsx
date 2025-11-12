510651762cab9bff0450f97d9c581ed74a49d9e5
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-11-12 09:13:00 +0100

    Implemented location tracking permission on click & added UserLocationTracker

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -3,14 +3,17 @@ import "leaflet/dist/leaflet.css";
 import { useEffect, useState } from "react";
 import L from "leaflet";
 import { leafletSettings } from "@/lib/leaflet.settings";
-import UserMarker from "../Markers/UserMarker";
 import UserLocationTracker from "../Tracker/UserLocationTracker";
 import { Button } from "../ui/button";
+import { useTrackLocation } from "@/hooks/useTrackLocation";
+import { useGeoLocPermission } from "@/hooks/useGeoLocPermission";
+import { cn } from "@/lib/utils";
 
 const Map = () => {
   const [markerPosition, setMarkerPosition] = useState<[number, number] | null>(
     null
   );
+  // const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(false);
 
   const START_POSITION: [number, number] = [50.0755, 14.4378];
 
@@ -28,10 +31,30 @@ const Map = () => {
     return null;
   }
 
+  // check permission from user to track location
+  const permissionStatus = useGeoLocPermission();
+
+  // coordinates from hook
+  const { coordinates, isTracking, startTracking } = useTrackLocation();
+
   // bybass react-leaflet error -> react-leaflet settings
   useEffect(() => {
     L.Icon.Default.mergeOptions({ leafletSettings });
   }, []);
+
+  let buttonText = "Allow GeoLocation";
+  let isButtonVisible = true;
+
+  if (isTracking && permissionStatus === "granted") {
+    buttonText = "Location Tracking Active";
+    isButtonVisible = false;
+  } else if (permissionStatus === "denied") {
+    buttonText = "Location Denied (Check Browser Settings)";
+    isButtonVisible = true;
+  } else if (permissionStatus === "unsupported") {
+    buttonText = "Geolocation Not Supported";
+    isButtonVisible = true;
+  }
 
   return (
     <div>
@@ -49,11 +72,17 @@ const Map = () => {
         />
 
         {/* {markerPosition && <UserMarker position={markerPosition} />} */}
-        <UserLocationTracker />
+        <UserLocationTracker coordinates={coordinates} isTracking />
         <MapClickHandler />
         <div className="relative z-999 h-full">
-          <Button className="absolute left-[50%] top-[85%] translate-x-[-50%] cursor-pointer">
-            Allow GeoLocation
+          <Button
+            className={cn(
+              `absolute left-[50%] top-[85%] translate-x-[-50%] cursor-pointer`,
+              isButtonVisible ? "block" : "hidden"
+            )}
+            onClick={startTracking}
+          >
+            {buttonText}
           </Button>
         </div>
       </MapContainer>
