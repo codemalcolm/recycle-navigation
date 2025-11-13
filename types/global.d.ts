99ee6b659f98071bcc8280213e01018f0b3756d9
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-11-13 21:34:58 +0100

    Added types of Osm data

--- types/global.d.ts
+++ types/global.d.ts
@@ -6,3 +6,31 @@ type CoordinatesObject = {
   lat: number | null;
   long: number | null;
 };
+
+type OsmObject = RecyclingContainer | WasteBasket;
+
+interface RecyclingContainer {
+  type: string;
+  id: number;
+  lat: number;
+  lon: number;
+  tags: Tags;
+}
+
+interface WasteBasket {
+  type: string;
+  id: number;
+  lat: number;
+  lon: number;
+  tags: Tags;
+}
+
+interface Tags {
+  amenity: string;
+  recycling_beverage_cartons?: string;
+  recyclingcooking_oil?: string;
+  recycling_glass?: string;
+  recycling_paper?: string;
+  recycling_plastic?: string;
+  recycling_type?: string;
+}
