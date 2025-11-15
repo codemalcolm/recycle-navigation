1baa3389e69330720556d82e91972262f2deed48
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-11-15 19:05:15 +0100

    Removed flyTo function as it is now implemented in MarkerPopup component

--- components/Markers/ClickedMarker.tsx
+++ components/Markers/ClickedMarker.tsx
@@ -9,21 +9,21 @@ type ClickedMarkerProps = {
 
 const ClickedMarker = ({ markerData }: ClickedMarkerProps) => {
   const router = useRouter();
-  const coordinates: Coordinates = [markerData.lat, markerData.lon];
+  const coordinates: Coordinates = [markerData.lat!, markerData.lon!];
   const searchParams = useSearchParams();
   const map = useMap();
 
   const eventHandlers = {
     click: () => {
       const currentSearchParams = new URLSearchParams(searchParams);
-      currentSearchParams.set("markerId", markerData?.id.toString()); // set new search parameter "markerId"
+      currentSearchParams.set("markerId", markerData.id!.toString()); // set new search parameter "markerId"
       console.log("Marker clicked:", markerData);
       // push the markerData id parameter into app router
       router.push(
         ROUTES.MARKER_SEARCH_PARAMS(currentSearchParams.toString() as string),
         { scroll: false }
       );
-      map.flyTo(coordinates, 15, { duration: 0.68 });
+      // map.flyTo(coordinates, 15, { duration: 0.68 });
     },
   };
 
