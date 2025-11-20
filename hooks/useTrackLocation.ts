06ffd1444eebf24717c84fab9a0d61e214d36334
Vojta Cvrcek <vojta.cvrcek1@gmail.com>
2025-11-20 18:08:33 +0100

    Changed hook to pass user coordinates to Context instead of returning the coordinates

--- hooks/useTrackLocation.ts
+++ hooks/useTrackLocation.ts
@@ -1,17 +1,21 @@
+import { useUserCoordinates } from "@/context/UserCoordinatesProvider";
 import { watchGeoLocation } from "@/lib/utils";
 import { Coordinates } from "@/types/global";
 import { useCallback, useRef, useState } from "react";
 
 export const useTrackLocation = () => {
-  const [coordinates, setCoordinates] = useState<Coordinates | null>(null);
+  const [, setContextCoordinates] = useUserCoordinates();
   const [isTracking, setIsTracking] = useState<boolean>(false);
 
   const stopTrackingRef = useRef<() => void | null>(null);
 
-  const handleLocationUpdate = useCallback((coords: Coordinates) => {
-    // The coords variable is already a valid LatLngExpression tuple
-    setCoordinates(coords);
-  }, []);
+  const handleLocationUpdate = useCallback(
+    (coords: Coordinates) => {
+      // The coords variable is already a valid LatLngExpression tuple
+      setContextCoordinates(coords);
+    },
+    [setContextCoordinates]
+  );
 
   const startTracking = () => {
     if (isTracking) return; // disallow more watches
@@ -32,7 +36,6 @@ export const useTrackLocation = () => {
   }, []);
 
   return {
-    coordinates,
     isTracking,
     startTracking,
     stopTracking,
